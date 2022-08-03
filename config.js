c1d617c3fc4e896396e58fb71ebde58adc7a36ea
jircdeveloper <jircdev@gmail.com>
2022-08-03 11:04:55 -0300

    host

--- config.js
+++ config.js
@@ -1,4 +1,20 @@
 /************************
-APPLICATION CONFIGURATION
-************************/
-globalThis.__beyond_config = {"distribution":{"local":true,"key":"1334341767","environment":"development","mode":"amd","baseDir":"/"},"application":{"package":"@beyond/web-tutorial","version":1,"languages":{"default":"en","supported":["en","es"]},"layout":"main-layout","params":{}},"backends":[["@beyond-js/local/legacy","http://localhost:3031"],["@beyond/web-tutorial/backend","http://localhost:3030"]]};
+ APPLICATION CONFIGURATION
+ ************************/
+globalThis.__beyond_config = {
+    "distribution": {
+        "local": true,
+        "key": "1334341767",
+        "environment": "development",
+        "mode": "amd",
+        "baseDir": "/"
+    },
+    "application": {
+        "package": "@beyond/web-tutorial",
+        "version": 1,
+        "languages": {"default": "en", "supported": ["en", "es"]},
+        "layout": "main-layout",
+        "params": {}
+    },
+    "backends": [["@beyond-js/local/legacy", "http://localhost:3031"], ["@beyond/web-tutorial/backend", "https://beyond-docs.herokuapp.com/"]]
+};
